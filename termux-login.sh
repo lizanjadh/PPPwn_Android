3ab0d671ea979ccf2c017b4d13f6de9994408593
lizanjadh <36481693+lizanjadh@users.noreply.github.com>
2024-05-31 12:56:54 -0600

    Update termux-login.sh

--- termux-login.sh
+++ termux-login.sh
@@ -2,13 +2,3 @@
 ## This script is sourced by /data/data/com.termux/files/usr/bin/login before executing shell.
 ##
 cd /data/data/com.termux/files/home/PPPwn_Android && sh start.sh
-cd /data/data/com.termux/files/home/PPPwn_Android && sh start.sh
-cd /data/data/com.termux/files/home/PPPwn_Android && sh start.sh
-cd /data/data/com.termux/files/home/PPPwn_Android && sh start.sh
-cd /data/data/com.termux/files/home/PPPwn_Android && sh start.sh
-cd /data/data/com.termux/files/home/PPPwn_Android && sh start.sh
-cd /data/data/com.termux/files/home/PPPwn_Android && sh start.sh
-cd /data/data/com.termux/files/home/PPPwn_Android && sh start.sh
-cd /data/data/com.termux/files/home/PPPwn_Android && sh start.sh
-cd /data/data/com.termux/files/home/PPPwn_Android && sh start.sh
-cd /data/data/com.termux/files/home/PPPwn_Android && sh start.sh
