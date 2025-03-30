b34a680596d0e67dc2f0fe2bf906f6e3240e41ba
lizanjadh <36481693+lizanjadh@users.noreply.github.com>
2025-03-30 16:35:24 -0600

    Add files via upload

--- start.sh
+++ start.sh
@@ -21,7 +21,7 @@ Fiuscha="\033[0;35m"
 blue="\033[1;34m"
 nc="\e[0m"
 
-echo "$blue __________________________________________________________"
+echo "$blue ________________________________________________"
 echo""
 
 
@@ -31,11 +31,13 @@ echo "$green _____  _____  _____
 | |__) | |__) | |__) |_      ___ __ _| |_ _| |_
 |  ___/|  ___/|  ___/\ \ /\ / / '_ \_   _|_   _|
 | |    | |    | |     \ V  V /| | | ||_|   |_|
-|_|    |_|    |_|      \_/\_/ |_| |_|.$yellow V2.1   $white By:$green Theflow0"
+|_|    |_|    |_|      \_/\_/ |_| |_|.
+"
+echo "$green $yellow V2.1   $white By:$green Theflow0"
 
 echo""
 
-echo "$blue __________________________________________________________"
+echo "$blue ________________________________________________"
 
 echo""
 
@@ -45,40 +47,40 @@ echo "$Yellow ㊂ $yellow Menu $green  PPPwn++ $yellow Exploit PS4 By Theflow0"
 
 echo ""
 
-echo "$green __________________________________________________________"
+echo "$green ________________________________________________"
 
 echo ""
 
 
-echo "$yellow 01. $white Start $white PPPwn++ $white Exploit  7.00 PS4 Firmware"
+echo "$yellow 01. $white Start $white PPPwn++ $white Exploit 7.00  PS4 Firmware"
 
 echo ""
 
-echo "$yellow 02. $white Start $white PPPwn++ $White Exploit 7.01 PS4 Firmware"
+echo "$yellow 02. $white Start $white PPPwn++ $White Exploit 7.01  PS4 Firmware"
 
 echo ""
 
-echo "$yellow 03. $white Start $white PPPwn++ $White Exploit 7.50 PS4 Firmware"
+echo "$yellow 03. $white Start $white PPPwn++ $White Exploit 7.50  PS4 Firmware"
 
 echo ""
 
-echo "$yellow 04. $white Start $white PPPwn++ $White Exploit 8.00 PS4 Firmware"
+echo "$yellow 04. $white Start $white PPPwn++ $White Exploit 8.00  PS4 Firmware"
 
 echo ""
 
-echo "$yellow 05. $white Start $white PPPwn++ $White Exploit 8.50 PS4 Firmware"
+echo "$yellow 05. $white Start $white PPPwn++ $White Exploit 8.50  PS4 Firmware"
 
 echo ""
 
-echo "$yellow 06. $white Start $white PPPwn++ $White Exploit 9.00 PS4 Firmware"
+echo "$yellow 06. $white Start $white PPPwn++ $White Exploit 9.00  PS4 Firmware"
 
 echo ""
 
-echo "$yellow 07. $white Start $white PPPwn++ $White Exploit 9.03 PS4 Firmware"
+echo "$yellow 07. $white Start $white PPPwn++ $White Exploit 9.03  PS4 Firmware"
 
 echo ""
 
-echo "$yellow 08. $white Start $white PPPwn++ $White Exploit 9.50 PS4 Firmware"
+echo "$yellow 08. $white Start $white PPPwn++ $White Exploit 9.50  PS4 Firmware"
 
 echo ""
 
@@ -94,19 +96,7 @@ echo "$yellow 11. $white Start $white PPPwn++ $White Exploit 11.00 PS4 Firmware"
 
 echo ""
 
-echo "$yellow 12. $white View Ethernet adapter speed"
-
-echo ""
-
-echo "$yellow 13. $white Change the speed of the Ethernet adapter to 1000 mb/s"
-
-echo ""
-
-echo "$yellow 14. $white Change the speed of the Ethernet adapter to 100 mb/s"
-
-echo ""
-
-echo "$yellow 15. $red Exit"
+echo "$yellow 12. $red Exit"
 
 echo ""
 
@@ -225,38 +215,6 @@ read foo
 sleep 1
 
 sh start_1100.sh
-
-read foo
-
-;;
-
-12) echo "$green Ethernet adapter speed is..."
-
-echo "$yellow"
-
-sleep 1
-
-sudo ethtool eth0 | grep Speed
-
-read foo
-
-;;
-
-13) echo "$green Adapter speed changed to 1000 mb/s..."
-
-sleep 1
-
-sudo ethtool -s eth0 speed 1000 duplex full
-
-read foo
-
-;;
-
-14)echo "$green Adapter speed changed to 100 mb/s..."
-
-sleep 1
-
-sudo ethtool -s eth0 speed 100 duplex full
 
 read foo
 
